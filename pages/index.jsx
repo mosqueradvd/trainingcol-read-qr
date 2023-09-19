9d141ced29c2d9d461fd84323628f5fc575cab44
David Mosquera <dmosqueram3@ucentral.edu.co>
2023-09-19 16:45:53 -0500

    fix: add input value to lowercase

--- pages/index.jsx
+++ pages/index.jsx
@@ -136,7 +136,7 @@ export default function Home() {
                     type="text"
                     name="username"
                     id="username"
-                    onChange={(e) => setUsername(e.target.value)}
+                    onChange={(e) => setUsername(e.target.value.toLowerCase())}
                     className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     placeholder="username"
                     required
